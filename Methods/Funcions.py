44e4fe3cff42135aad3e2f5f27f5e64b3cbd3c02
nroldanr <nroldanr@eafit.edu.co>
2020-10-14 01:03:41 -0500

    Fixed errors

---
+++ Methods/Funcions.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def f(x):
+    return np.log(np.power(np.sin(x),2) + 1) - 0.5
+
+def f1(x):
+    return np.log(np.power(np.sin(x),2)+1)-0.5-x
+
+def fPrime(x):
+    2 * ( 1 / (np.power(np.sin(x), 2) + 1)) * np.sin(x) * np.cos(x)
+
+def g(x):
+    return np.log(np.power(np.sin(x),2)+1)-1/2
+
+def h(x):
+    return np.exp(x)-x-1
+
+def hPrime(x):
+    return np.exp(x) - 1
+
+def hPrimeTwo(x):
+    return np.exp(x)
--- Methods/PuntoFijo.py
+++ Methods/PuntoFijo.py
@@ -1,36 +1,22 @@
 import math
-
 from scitools.StringFunction import StringFunction
 from math import *
-
-
-class Function:
-    def __init__(self, input):
-        self.function = StringFunction(input)
-        self.input = input
-
-    def evaluate(self, value):
-        return self.function(value)
-
-    def evaluate2(self, value):
-        x = value
-        return eval(self.input)
+import Funcions as fc
 
 class FixedPoint:
 
     def __init__(self):
         self.values = []
 
-    def evaluate(self, xi, tolerancia, iter, function, g_function, type_error=1):
+    def evaluate(self, x0, tolerancia, iter, function, g_function, type_error=0):
 
-        fun = Function(function)
-        gx = Function(g_function)
+        print("iter", "xi", "g(xi)", "f(xi)", "E")
 
-        fx = fun.evaluate2(xi)
+        fx = function(x0)
 
         if fx == 0:
-            print(f"{xi} is the root")
-            return f"{xi} is the root"
+            print(f"{x0} is the root")
+            return f"{x0} is the root"
         if iter < 1:
             print("The iterator value is wrong")
             return "The iterator value is wrong"
@@ -38,37 +24,41 @@ class FixedPoint:
             print("Tolerance error, must be greater than or equal to 0")
             return "Tolerance error, must be greater than or equal to 0"
 
-        self.values.append([0, str(xi), str("{:.2e}".format(fx)), None])
+        self.values.append([0, str(x0), str("{:.2e}".format(fx)), None])
         contador = 0
         error = tolerancia + 0.1
         while fx != 0 and error > tolerancia and contador < iter:
-            xn = gx.evaluate2(xi)
-            fi = fun.evaluate2(xn)
+            xn = g_function(x0)
+            fi = function(xn)
+            fn = function(x0)
+            print(contador, x0, xn, fn, error)
 
             if type_error == 0:
-                error = abs(xn-xi)
+                error = abs(xn-x0)
             else:
-                error = abs((xn-xi)/xn)
+                error = abs((xn-x0)/xn)
 
-            xi = xn
+            x0 = xn
 
             contador = contador + 1
+
             self.values.append([contador, str(xn), str(
                 "{:.2e}".format(fi)), str("{:.2e}".format(error))])
 
+
         if fx == 0:
-            print(f"{xi} is the root")
-            return f"{xi} is the root"
+            print(f"{x0} is the root")
+            return f"{x0} is the root"
         elif error < tolerancia:
-            print(f"{xi} is an aproximation with tolerance of {tolerancia}")
-            return f"{xi} is an aproximation with tolerance of {tolerancia}"
+            print(f"{x0} is an aproximation with tolerance of {tolerancia} and after {contador} iterations")
+            return f"{x0} is an aproximation with tolerance of {tolerancia} "
         else:
             print(f"Failed after {iter} iterations")
-            return f"Failed after {iter} iterations"
+            return f"Failed after {iter} iterations "
 
     def tabla_values(self):
         print(self.values)
         return self.values
 
 fpoint = FixedPoint()
-fpoint.evaluate(1, 0.1, 20, 2, 0, type_error=1)
+fpoint.evaluate(-0.5, 1E-7, 100, fc.f1, fc.g)
