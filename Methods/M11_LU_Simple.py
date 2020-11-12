1565b2ed939299462ba31802de746cf0f9499a3d
marami2 <marianar97@LAPTOP-J03EB5V8.localdomain>
2020-11-12 00:29:05 -0500

    english LU simple

--- Methods/M11_LU_Simple.py
+++ Methods/M11_LU_Simple.py
@@ -11,13 +11,13 @@ def LU(A):
     A = A.astype(np.float64)
     L = L.astype(np.float64)
     U = U.astype(np.float64)
-    print(f"Etapa 0 \n")
+    print(f"Simple LU\n Stage 0 \n")
     np.savetxt(sys.stdout, A, fmt="%8.3f")
     print()
 
 
     for etapa in range(0,n-1):
-        print(f"Etapa {etapa+1} \n")
+        print(f"Stage {etapa+1} \n")
         if A[etapa,etapa] == 0:
             print("A connot have 0 in its diagonal")
             return 1
@@ -25,9 +25,6 @@ def LU(A):
             if A[fila, etapa] != 0:
                 L[fila,etapa] = A[fila,etapa]/ A[etapa,etapa]
                 multiplicador = A[fila, etapa]/A[etapa,etapa]
-                uno = (A[fila,etapa::])
-                dos = A[etapa,etapa::]
-                tres = ((A[fila, etapa] / A[etapa, etapa]) * A[etapa, etapa::])
                 A[fila,etapa::] = (A[fila,etapa::] -((A[fila, etapa]/A[etapa,etapa]) * A[etapa,etapa::]))
 
         U[etapa, etapa:n+1] = A[etapa,etapa:n+1]
@@ -43,9 +40,9 @@ def LU(A):
         # print(U)
 
     U[-1,-1] = A[-1,-1]
-    print(A)
-    print("U matrix")
-    print(U)
+    # print(A)
+    # print("U matrix")
+    # print(U)
     return L, U
 
 def LU_simple(A,b):
@@ -53,29 +50,34 @@ def LU_simple(A,b):
     lb = np.concatenate((L,b),axis=1)
     z = prog_sust(lb)
     z = z.reshape((z.shape[0],1))
-    print("z")
-    print(z)
+    # print("z")
+    # print(z)
     
     uz = np.concatenate((U,z), axis=1)
-    print("Uz \n")
-    print(uz)
-    print("x")
+    # print("Uz \n")
+    # print(uz)
+    # print("x")
     x = sustitucion_regresiva(uz)
-    print(x)
+
+    print("After applying progresive and regresive sustitution\n X: ")
+    np.savetxt(sys.stdout, x.transpose(), fmt="%8.3f")
+
+
+    # print(x)
 
 A = np.array([
-    [4,3, -2, -7],
+    [4,3, -2, 7],
     [3,12,8,-3],
     [2,3,-9,3],
     [1,-2,-5,6]])
 
-# A = np.array([
-#     [4,-1, 0, 3],
-#     [1,15.5,3,8],
-#     [0,-1.3,-4,1.1],
-#     [14,5,-2, 30]])
+A = np.array([
+    [4,-1, 0, 3],
+    [1,15.5,3,8],
+    [0,-1.3,-4,1.1],
+    [14,5,-2, 30]])
 
-b = np.array([20 ,18 , 31, 12]).transpose().reshape(4,1)
+b = np.array([1 ,1 , 1, 1]).transpose().reshape(4,1)
 
 LU_simple(A,b)
 
