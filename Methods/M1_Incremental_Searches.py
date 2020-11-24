a75554cc2c36ecf80ba6dbd0f72cb670b712ba68
mcastri6 <71736693+mcastri6@users.noreply.github.com>
2020-11-24 16:36:32 -0500

    Update M1_Incremental_Searches.py
    update M1

--- Methods/M1_Incremental_Searches.py
+++ Methods/M1_Incremental_Searches.py
@@ -1,44 +1,42 @@
+
 from math import *
-print ("enter the function:")
-f = input()
-print("enter the initial value (xo):")
-x = float(input())
-print("what is the delta:")
-d = float(input())
-print ("chow many maximum iterations:")
-i = int(input())
-#print ("minimum tolerance:")
-#tol = float(input())
 
 
-def func():
-    res = eval(f)
-    return float(res)
-   
-def fun1():
-    x = xa
-    res = eval(f)
-    return res
+def Incrementalsearches(function,initialvalue,delta,iterations):
+   f=function
+   xo=initialvalue     
+   d=delta
+   i=iterations
 
-t=log(3.14)*2
-if func() == 0.0:
-    print (x,"is root")
-else:
-    xa = x + d
-    res1 = fun1()
-    cont = 1 
-    encontrado=False
-    while (cont < i):
-        x = xa
-        res1 = func()
-        xa = x + d
-        res1 = fun1()
-        cont = cont + 1
-        if res1 == 0:
-            print (xa,"s root")
-        else:
-            if (res1*func() < 0):
-                print ("There is a root of f in [",x,",",xa,"]")
-                encontrado=True
-if ((cont==i) and (not encontrado)):
-    print("With the number of requested iterations, no interval was found that could contain a root")
+   def func():
+       x=xo
+       res = eval(f)
+       return float(res)
+      
+   def fun1():
+       x=xa
+       res = eval(f)
+       return res
+
+   if func() == 0.0:
+       print (x,"is root")
+   else:
+       cont = 1 
+       encontrado=False
+       while (cont < i):
+           res1 = func()
+           xa = xo + d
+           res2 = fun1()
+           if res2 == 0:
+               print (xa,"is root")
+           else:
+               if (res1*res2 < 0):
+                   print ("There is a root of f in [","{:14.10f}".format(xo),",","{:14.10f}".format(xa),"]")
+                   encontrado=True
+           cont = cont + 1
+           xo=xa
+   if ((cont==i) and (not encontrado)):
+       print("With the number of requested iterations, no interval was found that could contain a root")
+
+Incrementalsearches('log(sin(x)**2+1)-1/2', -3, 0.5, 100)
+
